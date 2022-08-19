44976db886e19a43d4d7424deef7f6744b07762d
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-19 09:47:55 +0500

    task 34 is done

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -2,3 +2,42 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = random.Next(min, max);
+    }
+    return arr;
+}
+
+int PosEven(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        counter++;
+    }
+    return counter;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1]);
+    Console.Write("]");
+}
+
+int[] array = CreateArray(15, 100, 1000);
+PrintArray(array);
+Console.WriteLine();
+int posEven = PosEven(array);
+Console.WriteLine(posEven);
