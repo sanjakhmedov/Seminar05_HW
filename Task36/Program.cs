6b66608226de08566de304bdcba2248ed9100afc
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-19 10:06:41 +0500

    taks 36 is done

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -3,3 +3,21 @@
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] array = {2, 6, 4, 8, 9, 4, 3, 7, 12, 6, 3, 5};
+
+int OddIndexSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int oddSum = OddIndexSum(array);
+Console.WriteLine(oddSum);
